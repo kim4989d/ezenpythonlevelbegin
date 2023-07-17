2a9b8ed946fac22ec953b0c3b96b17a09299f4c2
len-kim <kim4989d@naver.com>
2023-07-17 19:28:01 +0900

    1.0

---
+++ common/while_1.py
@@ -0,0 +1,19 @@
+init=0
+
+
+result=0
+while init<=10:
+    result = result + init
+    init = init + 1
+
+    print(result)
+
+
+a=1
+b=10
+result=0
+for i in range(a,b+1):
+    result=result+i
+
+print("합은:",result)
+
