ded422af946c4d28c561e3e4264a5c04be3639af
kim4989d <kim4989d@naver.com>
2023-07-29 04:15:55 +0900

    1.2

--- common/global_1.py
+++ common/global_1.py
@@ -1,5 +1,12 @@
 class school:
+    global a
+    a=[]
+    global obinput
+    obinput=[]
 
+    @staticmethod
+    def printe(input):
+        print(input,end="\t")
 
     def __init__(self, roomnumber, roomtype, roomcheck):
         self.roomnumber = roomnumber
@@ -8,22 +15,28 @@ class school:
 
     @staticmethod
     def printok():
-        a = []
-        obinput=[]
+        global a
+
+
         while True:
-            inputvalue = input('룸번호 룸타입  룸 인원을 입력하세요 ex)room1301/html/5')
+            inputvalue = input('룸번호 룸타입  룸 인원을 입력하세요 ex)room1301/html/5 \n')
             sp = inputvalue.split('/')
+            if len(sp) !=3:
+                print("세자리를 입력 !!")
+            else:
+                for i in sp:
+                    a.append(i)
 
-            for i in sp:
-                a.append(i)
+                schob = school(a[0], a[1], a[2])
+                obinput.append(schob)
 
-            schob = school(a[0], a[1], a[2])
-            obinput.append(schob)
+                for i in obinput:
+                    print('=' * 100)
+                    school.printe(f'룸번호:{i.roomnumber}')
+                    school.printe(f'룸타입:{i.roomtype}')
+                    school.printe(f'룸인원:{i.roomcheck}')
+                    print()
+                    print('=' * 100)
 
-            for i in obinput:
-                print(i.roomnumber)
-                print(i.roomtype)
-                print(i.roomcheck)
-
-            a.clear()
+                a.clear()
 school.printok()
