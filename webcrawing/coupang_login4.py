8473688f787e75bbe00b4e48abf99f19a5b21597
김현호 <kim4989d@naver.com>
2023-09-10 17:49:23 +0900

    1.4

--- webcrawing/coupang_login4.py
+++ webcrawing/coupang_login4.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
 
+
 class coupang_login4:
 
     def CoupangList(self,userid,passwd):
@@ -35,7 +36,7 @@ class coupang_login4:
 
         # 로그인 후 페이지 로딩 대기 (적절한 방법으로 변경 가능)
         # driver.implicitly_wait(10)
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 5)
         wait.until(EC.presence_of_element_located((By.ID, 'headerSearchKeyword')))
         search_input = driver.find_element(By.XPATH, "//input[@id='headerSearchKeyword']")
         search_input.send_keys("노트북")
@@ -101,6 +102,7 @@ for i, printvalue in enumerate(productlist):
 # sleep(2)
 
 df = pd.DataFrame(productlist)
+
 
 
 # DataFrame을 Excel 파일로 저장하며, 열 인덱스는 제외하고 저장합니다 (index=False)
